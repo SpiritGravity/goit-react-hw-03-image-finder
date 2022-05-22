de9ef3c9c53ab165f4603e50aecc641904af8d4f
Anastasiia Tyshkevych <anastasia.tyshkevych@gmail.com>
2022-05-22 14:52:37 +0300

    upd-api-key

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,5 +1,5 @@
 function fetchImages(query, page) {
-    const API_KEY = '27160934-473c64884c6d78e81a1115d4e';
+    const API_KEY = '25313098-4fe256b98efcc708221cd816f';
     const URL = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   
     return fetch(URL).then(response => response.json());
